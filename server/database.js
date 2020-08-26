2494cfec607711a9634195c5a0a5ffc61802c756
Brandon <blstrzyzewski@gmail.com>
2020-08-25 20:58:48 -0400

    updated database for production

--- server/database.js
+++ server/database.js
@@ -1,25 +1,24 @@
-const util = require('util');
-const mysql = require ('mysql');
-const dotenv = require('dotenv');
+const util = require("util");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
 dotenv.config();
 
-function makeDb( ) {
-    const config={
-        host: process.env.DB_HOST,
-        user: "root",
-        password: process.env.DB_PASSWORD,
-        database:"weather_db"
-        }
-    const connection = mysql.createConnection( config );
-    return {
-      query( sql, args ) {
-        return util.promisify( connection.query )
-          .call( connection, sql, args );
-      },
-      close() {
-        return util.promisify( connection.end ).call( connection );
-      }
-    };
-  }
+function makeDb() {
+  const config = {
+    host: process.env.CLEARDB_HOST,
+    user: process.env.CLEARDB_USER,
+    password: process.env.CLEARDB_PASSWORD,
+    database: process.env.CLEARDB_DATABASE,
+  };
+  const connection = mysql.createPool(config);
+  return {
+    query(sql, args) {
+      return util.promisify(connection.query).call(connection, sql, args);
+    },
+    close() {
+      return util.promisify(connection.end).call(connection);
+    },
+  };
+}
 
-  module.exports = makeDb
+module.exports = makeDb;
