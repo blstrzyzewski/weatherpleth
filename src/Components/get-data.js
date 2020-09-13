6547bd11303be2402c788a3c2776bf83ef357767
blstrzyzewski <48768759+blstrzyzewski@users.noreply.github.com>
2020-09-12 20:22:54 -0400

    generalized DataList

--- src/Components/get-data.js
+++ src/Components/get-data.js
@@ -1,63 +1,29 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-
-function categorize(value){
-
-  if (value>80){
-    return "high"
-  }
-  if(value>40){
-    return "medium"
-  }
-  return "low"
-
-}
-
-function createPolygon(lon,lat){
-  let output=[]
-  output.push([lon+0.25,lat+0.25]);
-  output.push([lon+0.25,lat-0.25]);   
-  output.push([lon-0.25,lat-0.25]);
-  output.push([lon-0.25,lat+0.25]);
-  output.push([lon+0.25,lat+0.25]);
+export function createPolygon(lon, lat, size = 0.25) {
+  let output = [];
+  output.push([lon + size, lat + size]);
+  output.push([lon + size, lat - size]);
+  output.push([lon - size, lat - size]);
+  output.push([lon - size, lat + size]);
+  output.push([lon + size, lat + size]);
   return output;
 }
 
-export default async function DataList(value){
+export async function DataList(data, variable) {
+  const size = variable === "sst" ? 0.5 : 0.25;
 
-    //const [state,setState] =useState();
-    const options={
-      method: 'get',
-      url: '/api/data_points',
-      params:{
-      //dataset:value.value,
-      month:'DEC',
-      year:2012,
-      min:95,
-      max:2
-      } 
-    }
-    const res= await axios(options)
-    
-    let map=res.data.map((item)=>{
-        if (item.lat>90 || item.lat<-90){console.log("wrong")}
-        let polygon = createPolygon(item.lon,item.lat);
-           // console.log(polygon);
-        return {
-              "type": "Feature",
-              "properties": {"precipitation": categorize(item.cld)},
-              "geometry": {
-                  "type": "Polygon",
-                  "coordinates": [polygon]
-  
-                  
-              }
-      
-          }
-        });
-    
-    return map;
+ 
+  let map = data.map((item) => {
+    let polygon = createPolygon(item.lon, item.lat, size);
 
+    return {
+      type: "Feature",
+      properties: { [variable]: item[variable] },
+      geometry: {
+        type: "Polygon",
+        coordinates: [polygon],
+      },
+    };
+  });
+
+  return map;
 }
-
-
