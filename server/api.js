a894107c6899fbf0c0bcdbc2bc9ae08205c19b8a
blstrzyzewski <48768759+blstrzyzewski@users.noreply.github.com>
2020-09-15 17:32:42 -0400

    updated api routes

--- server/api.js
+++ server/api.js
@@ -4,10 +4,10 @@ const router= express.Router();
 const fs = require('fs')
 const db=makeDb();
 router.get('/geojson',async (req,res)=>{
-    console.log('route_working')
+    
     let id=req.query.id;
-   // res.send({"data":"Sdfasdf"})
-   const resultJSON= JSON.parse(fs.readFileSync(`../geojson/${id}.geojson`,'utf8'));
+  
+    const resultJSON= JSON.parse(fs.readFileSync(`../geojson/${id}.geojson`,'utf8'));
     
     res.send(resultJSON);
     
@@ -15,23 +15,45 @@ router.get('/geojson',async (req,res)=>{
   
   });
 
- router.get('/data_points',async function(req,res){
+router.get('/data_points',async function(req,res){
     
     
-    let variable=req.query.data_var;
-    console.log(variable);
+    let variable=req.query.data_var;  
     let month= req.query.month;
     let year = parseInt(req.query.year)
     let min = parseInt(req.query.min);
     let max= parseInt(req.query.max);
-   // let sql= `SELECT * FROM precipitation2 WHERE year=${year} AND month='${month}' AND pre>${min} AND pre<${max}`
-      let sql= `SELECT * FROM  ${variable} WHERE year=${year} AND month='${month}' `
-    console.log(sql)
-    const response= await db.query(sql)
+    let sql= `SELECT * FROM  ${variable} WHERE year=${year} AND month='${month}' `
     
-    //console.log(response);
+    const response= await db.query(sql)
     res.send(response);
     
-  })
+});
+
+router.get("/data_points_by_coords", async function (req, res) {
+  let variable = req.query.data_var;
+
+  let month = req.query.month;
+  let year = parseInt(req.query.year);
+  let lon = [parseFloat(req.query.lon[0]), parseFloat(req.query.lon[1])];
+  let lat = [parseFloat(req.query.lat[0]), parseFloat(req.query.lat[1])];
+  let sql = `SELECT * FROM pre_named WHERE month='${month}' AND year=${year} AND lat>${lat[0]} AND lat<${lat[1]} AND lon>${lon[0]} AND lon<${lon[1]}`;
+
+  const response = await db.query(sql);
+  res.send(response);
+});
+
+router.get("/data_points_by_location", async function (req, res) {
+  let variable = req.query.data_var;
+
+  let month = req.query.month;
+  let year = parseInt(req.query.year);
+  let location = req.query.location;
+
+  let sql = `SELECT * FROM ${variable}_named WHERE month='${month}' AND year=${year} AND location="${location}"`;
+
+  const response = await db.query(sql);
+  res.send(response);
+});
 
   module.exports=router;
